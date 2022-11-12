99e032b539e040133a3ffe2c362b24ee2d4c0733
thirteenaladdins <moamrr@gmail.com>
2022-11-12 20:13:28 +0000

    vpype add

--- vpype_optimisation.py
+++ vpype_optimisation.py
@@ -1,4 +1,19 @@
 import vpype_cli
+import sys
 
-vpype_cli.execute('read the_lover.svg write --pen-up the_lover_pen_up.svg')
-vpype_cli.execute('read the_lover.svg linesort write --pen-up the_lover_pen_up+linesort.svg')
+# input from command line
+
+if len(sys.argv) < 2:
+    print("Usage: python vpype_optimisation.py <filename>")
+    sys.exit(1)
+
+else:
+    input_file = sys.argv[1]
+
+
+
+# vpype_cli.execute('read the_lover.svg write --pen-up the_lover_pen_up.svg')
+# vpype_cli.execute('read the_lover.svg linesort write --pen-up the_lover_pen_up+linesort.svg')
+
+
+vpype_cli.execute(f'read {input_file} scaleto 15cm 15cm linesort write --page-size 15x15cm --center {input_file}_output')
